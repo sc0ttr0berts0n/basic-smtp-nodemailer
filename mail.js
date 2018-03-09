d021183384f9b2f6af54f78bc4ac2408a4adb8cc
Scott Robertson <robertsons@marketingresults.net>
2018-03-09 17:09:44 -0500

    took measures to make username and password private from the public repo

---
+++ mail.js
@@ -0,0 +1,80 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const pathToEmail = process.argv[2];
+const pathToPassword = '.password';
+let password = null;
+
+const pathToUsername = '.username';
+let username = null;
+
+function init() {
+    getUsername();
+}
+
+function getUsername() {
+    fs.readFile(pathToUsername, (err, data) => {
+        if (err) {
+            console.log(
+                'You probably need to add a ".password" file to doc root'
+            );
+            throw err;
+        }
+        password = data;
+        getPassword();
+    });
+}
+
+function getPassword() {
+    fs.readFile(pathToPassword, (err, data) => {
+        if (err) {
+            console.log(
+                'You probably need to add a ".password" file to doc root'
+            );
+            throw err;
+        }
+        password = data;
+        getHtmlFile();
+    });
+}
+
+function getHtmlFile() {
+    fs.readFile(pathToEmail, (err, data) => {
+        if (err) {
+            throw err;
+        }
+        sendEmail(data, password);
+    });
+}
+
+function sendEmail(data, password) {
+    let transporter = nodemailer.createTransport({
+        host: 'box1023.bluehost.com',
+        port: 465,
+        secure: true, // upgrade later with STARTTLS
+        auth: {
+            user: username,
+            pass: password
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: 'scott@sr28.net', // sender address
+        to:
+            'mktresults@gmail.com, mktresults@hotmail.com, marketingresults@yahoo.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: data // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+    });
+}
+
+init();
